38b508ea452ae52849db1d19730ea98c67182981
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2022-02-15 20:16:08 +0000

    can place card onto pile

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,57 @@
-export function greeter(person: string) {
-  return 'Hello, Rick'
+export type Card = {}
+
+type Snap = {
+  pile: Set<Card>,
+  player1Hand: Set<Card>,
+  player2Hand: Set<Card>,
 }
+
+type OpenPlay = Snap & {
+  placeCard: (player: Player) => Snap
+}
+
+type Player = 'player1' | 'player2'
+
+export const startGame = (deck: Set<Card>, totalPlayers: number = 2): OpenPlay => {
+  const [ player1Hand, player2Hand ] = dealCards(deck, totalPlayers)
+
+  const pile = new Set<Card>()
+
+  const initialState = {
+    pile,
+    player1Hand,
+    player2Hand,
+  }
+  return {
+    ...initialState,
+    placeCard: getPlaceCard(initialState)
+  }
+}
+
+const getPlaceCard = (state: Snap): (player: Player) => OpenPlay => {
+  return (player: Player) => {
+    const [card, ...hand] = Array.from(state.player1Hand.values())
+    const newState = {
+      pile: state.pile.add(card),
+      player1Hand: new Set(hand),
+      player2Hand: state.player2Hand
+    }
+
+    return {
+      ...newState,
+      placeCard: getPlaceCard(newState)
+    }
+  }
+}
+
+const dealCards = (deck: Set<Card>, totalPlayers: number) => {
+  const hands = new Array(totalPlayers)
+    .fill(undefined)
+    .map(_ => new Set<Card>())
+  Array.from(deck.values()).forEach((c, i) => {
+    const handIndex = i % totalPlayers
+    hands[handIndex].add(c)
+  })
+  return hands
+}
+
--- test/testOne.test.ts
+++ test/testOne.test.ts
@@ -1,5 +1,34 @@
-import { greeter } from '../src/example'
+import { startGame } from "../src/example"
 
-test('greeter uses the correct name', () => {
-    expect(greeter('Rick and Morty')).toBe('Hello, Rick and Morty');
-});
+describe('snap', () => {
+    const deck = new Set([
+        { suit : 'Hearts', value: 'A' },
+        { suit : 'Spades', value: 'A' },
+    ])
+    const game = startGame(deck, 2)
+
+    describe('starting the game', () => {
+        const { player1Hand, player2Hand} = game
+
+        it('should start with an empty pile', () => {
+            expect(game.pile.size).toEqual(0)
+        })
+
+        it('should deal the half the deck to each player', () => {
+            expect(player1Hand.size).toEqual(deck.size / 2)
+            expect(player1Hand.size).toEqual(player2Hand.size)
+            expect(player1Hand).not.toEqual(player2Hand)
+        })
+    })
+
+    describe('placing cards', () => {
+        it('should move that card onto the pile', () => {
+            const result = game.placeCard('player1')
+
+            expect(result.pile.size).toEqual(1)
+            const playedCard = { suit: 'Hearts', value: 'A' }
+            expect(result.pile.values().next().value).toEqual(playedCard)
+            expect(result.player1Hand.has(playedCard)).toBeFalsy()
+        })
+    })
+})
